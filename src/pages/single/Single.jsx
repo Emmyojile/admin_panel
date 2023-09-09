7786d32813ef7c59c9dc6f898eb635576b8e74e1
Emmyojile <emmaojile99@gmail.com>
2023-09-09 15:33:24 -0800

    tables, charts and sinngle page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,28 @@
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.scss";
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="itemImg" />
+              <div className="details">details</div>
+            </div>
+          </div>
+          <div className="right"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Single
+export default Single;
